e62ecfd89f6fbbe425bdf88835118ad7497a0b04
CatC0de1 <catcode0101@gmail.com>
2026-01-22 21:16:51 +0700

    perf: using redis for store throttle

--- src/core/throttle.module.ts
+++ src/core/throttle.module.ts
@@ -1,16 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
+import Redis from 'ioredis';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot({
-      throttlers: [
-        {
-          ttl: 60_000,
-          limit: 100,
-        },
-      ],
-      errorMessage: 'Limit Reached, Try Again Later',
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        throttlers: [
+          {
+            name: 'default',
+            ttl: 60_000,
+            limit: 100,
+          },
+        ],
+
+        errorMessage: 'Limit Reached, Try Again Later',
+
+        storage: new ThrottlerStorageRedisService(
+          new Redis({
+            keyPrefix: 'throttle:',
+            host: config.get<string>('REDIS_HOST'),
+            port: config.get<number>('REDIS_PORT'),
+            password: config.get<string>('REDIS_PASSWORD'),
+          }),
+        ),
+      }),
     }),
   ],
 })
