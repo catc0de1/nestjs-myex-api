ba03dc9a294c53c0aa964914a198449dc7c1cfdb
CatC0de1 <catcode0101@gmail.com>
2026-01-22 20:02:27 +0700

    feat: update strict of helmet config

--- src/configs/helmet.config.ts
+++ src/configs/helmet.config.ts
@@ -2,12 +2,26 @@ import helmet from 'helmet';
 
 export function HelmetConfig() {
   return helmet({
-    // pdf frame handler
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        frameSrc: ["'self'", 'blob:'],
-      },
-    },
+    contentSecurityPolicy: false,
+
+    // pure API = no client
+    // contentSecurityPolicy: {
+    //   directives: {
+    //     defaultSrc: ["'self'"],
+    //     scriptSrc: ["'self'"],
+    //     styleSrc: ["'self'"],
+    //     imgSrc: ["'self'", 'data:'],
+    //     frameSrc: ["'self'"],
+    //     objectSrc: ["'none'"],
+    //     baseUri: ["'self'"],
+    //     upgradeInsecureRequests: [],
+    //   },
+    // },
+
+    frameguard: { action: 'deny' },
+    hidePoweredBy: true,
+    referrerPolicy: { policy: 'no-referrer' },
+    noSniff: true,
+    xssFilter: false,
   });
 }
