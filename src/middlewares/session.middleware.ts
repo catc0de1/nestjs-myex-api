bd8b3e990e20261787e64cf3b5b76f20a703123c
CatC0de1 <catcode0101@gmail.com>
2026-01-23 18:37:19 +0700

    fix: remove redis in session and using ioredis

--- src/middlewares/session.middleware.ts
+++ src/middlewares/session.middleware.ts
@@ -1,22 +1,22 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import session from 'express-session';
 import { RedisStore } from 'connect-redis';
-import { createClient } from 'redis';
+import Redis from 'ioredis';
+import * as session from 'express-session';
 
-import type { Request, Response, NextFunction, RequestHandler } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class SessionMiddleware implements NestMiddleware {
-  private sessionMiddleware: RequestHandler;
+  private sessionMiddleware: ReturnType<typeof session>;
 
   constructor(private readonly configService: ConfigService) {
-    const redisClient = createClient({
-      socket: {
-        host: this.configService.get<string>('REDIS_HOST'),
-        port: this.configService.get<number>('REDIS_PORT'),
-      },
+    const redisClient = new Redis({
+      host: this.configService.get<string>('REDIS_HOST'),
+      port: this.configService.get<number>('REDIS_PORT'),
       password: this.configService.get<string>('REDIS_PASSWORD'),
+      lazyConnect: true,
+      maxRetriesPerRequest: null,
     });
 
     redisClient.connect().catch(console.error);
@@ -24,6 +24,7 @@ export class SessionMiddleware implements NestMiddleware {
     const store = new RedisStore({
       client: redisClient,
       prefix: 'sess:',
+      ttl: Math.floor(this.configService.getOrThrow<number>('SESSION_TTL')),
     });
 
     this.sessionMiddleware = session({
@@ -31,10 +32,12 @@ export class SessionMiddleware implements NestMiddleware {
       secret: this.configService.get<string>('SESSION_SECRET')!,
       resave: false,
       saveUninitialized: false,
+      rolling: true,
       cookie: {
         httpOnly: true,
         maxAge: this.configService.get<number>('SESSION_TTL'),
-        secure: false,
+        secure: this.configService.get<string>('NODE_ENV') === 'production',
+        sameSite: 'lax',
       },
     });
   }
