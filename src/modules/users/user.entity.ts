a4f694279cbff681274900df4c6857165273abc4
CatC0de1 <catcode0101@gmail.com>
2026-01-23 20:27:59 +0700

    fix: remove lifecycle hooks typeorm

--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -1,12 +1,4 @@
-import {
-  AfterInsert,
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  AfterUpdate,
-  AfterRemove,
-  OneToMany,
-} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Item } from '@/modules/items/item.entity';
 
 @Entity()
@@ -28,19 +20,4 @@ export class User {
 
   @OneToMany(() => Item, (item) => item.user)
   items: Item[];
-
-  @AfterInsert()
-  logInsert() {
-    console.log(`User with ID ${this.id} has been inserted.`);
-  }
-
-  @AfterUpdate()
-  logUpdate() {
-    console.log(`User with ID ${this.id} has been updated.`);
-  }
-
-  @AfterRemove()
-  logRemove() {
-    console.log(`User with ID ${this.id} has been removed.`);
-  }
 }
